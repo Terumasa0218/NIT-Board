d719ed3c011cf580e1a304d0e8caa6c67f23d651
Terumasa0218 <p.sh.ine.46315@gmail.com>
2025-12-21 14:13:23 +0900

    Add build metadata visibility and caching controls

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,6 +8,8 @@ declare interface ImportMetaEnv {
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
   readonly VITE_FIREBASE_APP_ID: string
   readonly VITE_FIREBASE_MEASUREMENT_ID?: string
+  readonly VITE_BUILD_SHA?: string
+  readonly VITE_BUILD_TIME?: string
 }
 
 declare interface ImportMeta {
